08ef5f04b66e4fa2c89746794b8974d33e9bde5c
Camila-TS <camilathompson@gmail.com>
2022-07-05 18:06:01 -0400

    comenzando con JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,9 @@
 const cipher = {
-  // ...
+  // let string='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  // 
+  // (x-65+n)%26+65
+  // cipher.encode(3, string)
+  // cipher.decode(3, string)
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+document.getElementById("aplicar").addEventListener("click", Cifrar)
+let aplicar = document.getElementById('aplicar').value;
+
+
+function Cifrar(){
+//texto original a cifrar
+let text1 = document.getElementById("texto1").value;
+//document.getElementById("texto1").value = "";
+
+let offSet = document.getElementById("número").value;
+}
+
+function modifyText () {
+text1
+}
+//function load (){
+    
+    //en este tiene que ir el texto cifrado, no permitir tipear
+let text1Done = document.getElementById("cifrado").value;
+   // text1Done.addEventListener("click", modifyText, false);
+//}
+//document.addEventListener("DOMContentLoaded", load, false);
+
+
+
+
+
+//textContent sino
+
+//function modifyText() {
+   // let t2 = document.getElementById("t2");
+   // t2.firstChild.nodeValue = "three";
+  //}
+
+  //function load() {
+   // let el = document.getElementById("t");
+   // el.addEventListener("click", modifyText, false);
+  //}
+
+  //document.addEventListener("DOMContentLoaded", load, false);
