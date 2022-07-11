efb04dcc2de05f990518f35fedd5ba8ceb326817
Camila-TS <camilathompson@gmail.com>
2022-07-11 09:57:26 -0400

    código funcionando en JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,21 @@
 const cipher = {
 
-  function encode(originalText, offSet){
+   encode: function (offSet, originalText){
     if (!originalText)
     return "";
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     offSet = (offSet % 26 + 26) % 26;
     return originalText.replace(/[A-Z]/ig, c => letters[(letters.indexOf(c) + offSet) % 26]);
-  }
+  },
 
-  function decode (originalText2, offSet){
+  decode: function (offSet, originalText2){
     if (!originalText2)
     return "";
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     offSet = (offSet % 26 - 26) %26;
     return originalText2.replace(/[A-Z]/ig, c=> letters[(letters.indexOf(c)-offSet)%26]);
-}
+  }
 
-  // let letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  // 
-  // (x-65+n)%26+65
-  // cipher.encode(3, string)
-  // cipher.decode(3, string)
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -3,31 +3,29 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 function inicio() {
-   document.getElementById("texto1").addEventListener("keyup", function() {
+   document.getElementById("texto1").addEventListener("keyup", function () {
       this.value = this.value.toUpperCase();
    }, true);
-   document.getElementById("texto2").addEventListener("keyup", function() {
+   document.getElementById("texto2").addEventListener("keyup", function () {
       this.value = this.value.toUpperCase();
    }, true);
 }
 inicio();
 
 
-
-//function configurar(offSet) {
-//document.getElementById("aplicar").addEventListener("click");
-
-//const offSet = document.getElementById("número").value;
-//}
-//configurar();
-
-document.getElementById("cifrar").addEventListener("click",function(){  
+document.getElementById("cifrar").addEventListener("click", function () {
    const originalText = document.getElementById("texto1").value;
-   
-   const offSet = document.getElementById("número").value;    
+
+   const offSet = document.getElementById("número").value;
    //ahí se muestra el texto cifrado           
-   document.getElementById("cifrado").value = cipher.encode(originalText, offSet);
-},true);
+   document.getElementById("cifrado").value = cipher.encode(offSet, originalText);
+}, true);
 
 
-   
+document.getElementById("descifrar").addEventListener("click", function () {
+   const originalText2 = document.getElementById("texto2").value;
+
+   const offSet = document.getElementById("número").value;
+   //ahí se muestra el texto cifrado           
+   document.getElementById("descifrado").value = cipher.decode(offSet, originalText2);
+}, true);
