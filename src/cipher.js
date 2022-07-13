e8c4773698108317ade5300920ae9f752cec582a
Camila-TS <camilathompson@gmail.com>
2022-07-13 14:40:27 -0400

    readme con imágenes

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,22 @@
 const cipher = {
 
    encode: function (offSet, originalText){
-    if (!originalText)
-    return "";
+    if (!originalText) {
+    let error = new TypeError("No hay texto ingresado")
+    return error;
+    }
+    //if (!originalText) {
+     // let error = new TypeError("No es texto")
+     // function typeOfText (originalText) {
+     // if (typeof originalText === "string"){
+     //   return (originalText);
+     // } else {
+     //   return error;
+     // }
+     // }
+     // typeOfText();
+    //}
+    
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     offSet = (offSet % 26 + 26) % 26;
     return originalText.replace(/[A-Z]/ig, c => letters[(letters.indexOf(c) + offSet) % 26]);
@@ -14,7 +28,26 @@ const cipher = {
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     offSet = (offSet % 26 - 26) %26;
     return originalText2.replace(/[A-Z]/ig, c=> letters[(letters.indexOf(c)-offSet)%26]);
-  }
+  },
+
+  //typeOfText: function (originalText) {
+    //if(typeof originalText === "string"){
+      //return (originalText)
+    //} else {
+      //return error
+    //}
+  //},
+
+  //typeOfText2: function (originalText2) {
+    //console.log(typeof originalText2, originalText2)
+    //if(typeof originalText2 === "string"){
+      //console.log(originalText2)
+    //} else {
+      //console.log("No es texto")
+    //}
+  //}
+
+
 
 };
 
